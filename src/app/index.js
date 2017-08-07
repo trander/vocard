e53b78d519a94b2c13c78feb9681773ac775adcc
Hyunjung <trlee329@gmail.com>
2017-08-07 20:48:46 +0200

    set stsate

--- src/app/index.js
+++ src/app/index.js
@@ -3,12 +3,35 @@ var ReactDOM = require('react-dom');
 
 // Create component
 var VocaComponent = React.createClass({
+  getInitialState : function() {
+    return {
+      vocas: ['apple','boy', 'cinammon'],
+      level: 3
+    }
+  },
   render: function() {
+    // this 
+    var levelr = setTimeout(function() {
+      this.setState({
+        level: 5
+      });
+    }.bind(this), 5000);
+
+
     return(
-      <h1>Hellooooooo</h1>
+      <div id='voca-list'>
+        <p>Vocabulary.</p>
+        <p>{this.state.level}</p>
+        <ul>
+          <li>{this.state.vocas[0]}</li>
+          <li>{this.state.vocas[1]}</li>
+          <li>{this.state.vocas[2]}</li>
+        </ul>
+      </div>
     );
   }
 });
+
 
 // put component into html page
 ReactDOM.render(
