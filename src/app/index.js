a0f648cd217cf58737d0bc69992308f788cd5ca8
Hyunjung <trlee329@gmail.com>
2017-08-07 15:47:33 +0200

    create component test

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,17 @@
-alert('it works!');
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-// require()
+// Create component
+var VocaComponent = React.createClass({
+  render: function() {
+    return(
+      <h1>Hellooooooo</h1>
+    );
+  }
+});
+
+// put component into html page
+ReactDOM.render(
+  <VocaComponent />,
+  document.getElementById('voca-wrapper')
+);
